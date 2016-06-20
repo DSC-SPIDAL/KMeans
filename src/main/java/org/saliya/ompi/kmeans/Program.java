0e2021493e784436a2e63f779e82699decfef686
Saliya Ekanayake <esaliya@gmail.com>
2016-06-20 00:47:05 -0400

    damn! threads don't work. now back to debugs

--- src/main/java/org/saliya/ompi/kmeans/Program.java
+++ src/main/java/org/saliya/ompi/kmeans/Program.java
@@ -142,9 +142,13 @@ public class Program {
                                 pointOffset);
 
                         // TODO - debugs
-                       /* if (finalItrCount ==1 && (ParallelOps.worldProcsCount > 1 ? ParallelOps.worldProcRank == 1 : ParallelOps.worldProcRank == 0)){
-                            System.out.println("point " + i  + " closest center "  +centerWithMinDist);
-                        }*/
+                        if (finalItrCount ==1 &&
+                                (ParallelOps.worldProcsCount > 1 ? ParallelOps.worldProcRank == 1 : ParallelOps.worldProcRank == 0) &&
+                                (numThreads > 1 ? threadIdx == 1 : threadIdx == 0)){
+                            System.out.println(
+                                    "Rank: " + ParallelOps.worldProcRank + " threadIdx: " + threadIdx + " point " + i +
+                                            " closest center " + centerWithMinDist);
+                        }
                         int centerOffset = threadIdx*numCenters*(dimension+1) + centerWithMinDist*(dimension+1);
                         ++centerSumsAndCountsForThread[centerOffset+dimension];
                         accumulate(points, centerSumsAndCountsForThread, pointOffset, centerOffset, dimension);
