196fbaf47449716f62e58153f0bc26280d8bc1b3
Saliya Ekanayake <esaliya@gmail.com>
2016-06-21 23:47:58 -0400

    adding lrt for kmeans

--- src/main/java/org/saliya/ompi/kmeans/ProgramWorker.java
+++ src/main/java/org/saliya/ompi/kmeans/ProgramWorker.java
@@ -59,7 +59,6 @@ public class ProgramWorker {
                 // Sum over threads
                 // Place results to arrays of thread 0
                 threadComm.sumDoubleArrayOverThreads(threadIdx, centerSumsAndCountsForThread);
-                System.out.println("Rank: " + ParallelOps.worldProcRank + " Thread: " + threadIdx + " came after thread sum");
             }
 
             if (ParallelOps.worldProcsCount > 1 && threadIdx == 0) {
@@ -68,7 +67,6 @@ public class ProgramWorker {
 
             if (numThreads > 1){
                 threadComm.broadcastDoubleArrayOverThreads(threadIdx, centerSumsAndCountsForThread, 0);
-                System.out.println("Rank: " + ParallelOps.worldProcRank + " Thread: " + threadIdx + " came after thread bcast");
             }
 
             converged = true;
