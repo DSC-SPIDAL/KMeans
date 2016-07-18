3eaa4c9f41e7dc9333903187777db0ac924baee5
Saliya Ekanayake <sekanaya@indiana.edu>
2016-07-18 16:57:21 -0400

    changing scripts

--- lrt.run.all.sh
+++ lrt.run.all.sh
@@ -8,6 +8,7 @@ nodes=16
 name="$nodes"n
 nodefile=nodes.$name.txt
 
+:<<COMMENT
 ./lrt.run.generic.sh 1 24 $nodefile $nodes 1 g $explicitbind $procbind
 list.cmd.sh $nodefile "rm -rf /dev/shm/$USER/*.bin /dev/shm/*.bin"
 ./lrt.run.generic.sh 2 12 $nodefile $nodes 2 g $explicitbind $procbind
@@ -20,9 +21,8 @@ list.cmd.sh $nodefile "rm -rf /dev/shm/$USER/*.bin /dev/shm/*.bin"
 list.cmd.sh $nodefile "rm -rf /dev/shm/$USER/*.bin /dev/shm/*.bin"
 ./lrt.run.generic.sh 8 3 $nodefile $nodes 8 g $explicitbind $procbind
 list.cmd.sh $nodefile "rm -rf /dev/shm/$USER/*.bin /dev/shm/*.bin"
+COMMENT
 ./lrt.run.generic.sh 12 2 $nodefile $nodes 12 g $explicitbind $procbind
 list.cmd.sh $nodefile "rm -rf /dev/shm/$USER/*.bin /dev/shm/*.bin"
-:<<COMMENT
 ./lrt.run.generic.sh 24 1 $nodefile $nodes 24 g $explicitbind $procbind
 list.cmd.sh $nodefile "rm -rf /dev/shm/$USER/*.bin /dev/shm/*.bin"
-COMMENT
--- lrt.run.generic.sh
+++ lrt.run.generic.sh
@@ -2,8 +2,10 @@
 cp=$HOME/.m2/repository/com/google/guava/guava/19.0/guava-19.0.jar:$HOME/.m2/repository/com/intellij/annotations/12.0/annotations-12.0.jar:$HOME/sali/software/jdk1.8.0/jre/../lib/tools.jar:$HOME/.m2/repository/commons-cli/commons-cli/1.2/commons-cli-1.2.jar:$HOME/.m2/repository/habanero-java-lib/habanero-java-lib/0.1.4-SNAPSHOT/habanero-java-lib-0.1.4-SNAPSHOT.jar:$HOME/.m2/repository/net/java/dev/jna/jna/4.1.0/jna-4.1.0.jar:$HOME/.m2/repository/net/java/dev/jna/jna-platform/4.1.0/jna-platform-4.1.0.jar:$HOME/.m2/repository/net/openhft/affinity/3.0/affinity-3.0.jar:$HOME/.m2/repository/net/openhft/compiler/2.2.4/compiler-2.2.4.jar:$HOME/.m2/repository/net/openhft/lang/6.8.2/lang-6.8.2.jar:$HOME/.m2/repository/ompi/ompijavabinding/1.10.1/ompijavabinding-1.10.1.jar:$HOME/.m2/repository/org/kohsuke/jetbrains/annotations/9.0/annotations-9.0.jar:$HOME/.m2/repository/org/ow2/asm/asm/5.0.4/asm-5.0.4.jar:$HOME/.m2/repository/org/slf4j/slf4j-api/1.7.12/slf4j-api-1.7.12.jar:$HOME/.m2/repository/org/xerial/snappy/snappy-java/1.1.2.1/snappy-java-1.1.2.1.jar:$HOME/sali/git/github/esaliya/java/KMeans/target/kmeans-1.0-spidal-lrt.jar:~/sali/software/slf4j-1.7.21/slf4j-api-1.7.21.jar:~/sali/software/slf4j-1.7.21/slf4j-simple-1.7.21.jar
 
 
-wd=$HOME/sali/projects/flink/kmeans/data2/1000000_1000
-#wd=/N/u/sekanaya/sali/git/github/esaliya/java/KMeans
+
+:<<COMMENT
+#original data
+wd=/N/u/sekanaya/sali/git/github/esaliya/java/KMeans
 c=$wd/centers.bin
 p=$wd/points.bin
 #p=$wd/points_X2.bin
@@ -12,6 +14,22 @@ n=1000000
 d=2
 k=1000
 m=1000
+t=0.00001
+b=true
+T=$1
+COMMENT
+
+wd=/N/u/sekanaya/sali/projects/flink/kmeans/data2
+points=1000000
+centers=500000
+
+exp="$points"_"$centers"
+p=$wd/$exp/points_BE.bin
+c=$wd/$exp/centers_BE.bin
+n=$points
+d=2
+k=$centers
+m=100
 t=0.00001
 b=true
 T=$1
