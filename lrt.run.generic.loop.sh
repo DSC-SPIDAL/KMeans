c9a3b567997670fa75a7daf08d4deb7697c51641
Saliya Ekanayake <sekanaya@indiana.edu>
2016-07-27 15:48:20 -0400

    changing scripts

--- lrt.run.generic.loop.sh
+++ lrt.run.generic.loop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp=$HOME/.m2/repository/com/google/guava/guava/19.0/guava-19.0.jar:$HOME/.m2/repository/com/intellij/annotations/12.0/annotations-12.0.jar:$HOME/sali/software/jdk1.8.0/jre/../lib/tools.jar:$HOME/.m2/repository/commons-cli/commons-cli/1.2/commons-cli-1.2.jar:$HOME/.m2/repository/habanero-java-lib/habanero-java-lib/0.1.4-SNAPSHOT/habanero-java-lib-0.1.4-SNAPSHOT.jar:$HOME/.m2/repository/net/java/dev/jna/jna/4.1.0/jna-4.1.0.jar:$HOME/.m2/repository/net/java/dev/jna/jna-platform/4.1.0/jna-platform-4.1.0.jar:$HOME/.m2/repository/net/openhft/affinity/3.0/affinity-3.0.jar:$HOME/.m2/repository/net/openhft/compiler/2.2.4/compiler-2.2.4.jar:$HOME/.m2/repository/net/openhft/lang/6.8.2/lang-6.8.2.jar:$HOME/.m2/repository/ompi/ompijavabinding/1.10.1/ompijavabinding-1.10.1.jar:$HOME/.m2/repository/org/kohsuke/jetbrains/annotations/9.0/annotations-9.0.jar:$HOME/.m2/repository/org/ow2/asm/asm/5.0.4/asm-5.0.4.jar:$HOME/.m2/repository/org/slf4j/slf4j-api/1.7.12/slf4j-api-1.7.12.jar:$HOME/.m2/repository/org/xerial/snappy/snappy-java/1.1.2.1/snappy-java-1.1.2.1.jar:$HOME/sali/git/github/esaliya/java/KMeans/target/kmeans-1.0-spidal-lrt.jar:~/sali/software/slf4j-1.7.21/slf4j-api-1.7.21.jar:~/sali/software/slf4j-1.7.21/slf4j-simple-1.7.21.jar
+cp=$HOME/.m2/repository/com/google/guava/guava/19.0/guava-19.0.jar:$HOME/.m2/repository/com/intellij/annotations/12.0/annotations-12.0.jar:$HOME/sali/software/jdk1.8.0/jre/../lib/tools.jar:$HOME/.m2/repository/commons-cli/commons-cli/1.2/commons-cli-1.2.jar:$HOME/.m2/repository/habanero-java-lib/habanero-java-lib/0.1.4-SNAPSHOT/habanero-java-lib-0.1.4-SNAPSHOT.jar:$HOME/.m2/repository/net/java/dev/jna/jna/4.2.1/jna-4.2.1.jar:$HOME/.m2/repository/net/java/dev/jna/jna-platform/4.2.1/jna-platform-4.2.1.jar:$HOME/.m2/repository/net/openhft/affinity/3.0/affinity-3.0.jar:$HOME/.m2/repository/net/openhft/compiler/2.2.4/compiler-2.2.4.jar:$HOME/.m2/repository/net/openhft/lang/6.8.2/lang-6.8.2.jar:$HOME/.m2/repository/ompi/ompijavabinding/1.10.1/ompijavabinding-1.10.1.jar:$HOME/.m2/repository/org/kohsuke/jetbrains/annotations/9.0/annotations-9.0.jar:$HOME/.m2/repository/org/ow2/asm/asm/5.0.4/asm-5.0.4.jar:$HOME/.m2/repository/org/slf4j/slf4j-api/1.7.12/slf4j-api-1.7.12.jar:$HOME/.m2/repository/org/xerial/snappy/snappy-java/1.1.2.1/snappy-java-1.1.2.1.jar:$HOME/sali/git/github/esaliya/java/KMeans/target/kmeans-1.0-spidal-lrt.jar:~/sali/software/slf4j-1.7.21/slf4j-api-1.7.21.jar:~/sali/software/slf4j-1.7.21/slf4j-simple-1.7.21.jar
 
 
 
@@ -19,8 +19,8 @@ b=true
 T=$1
 COMMENT
 
-#centers=(1000 10000 50000 100000 500000)
-centers=(10000)
+centers=(1000 10000 50000 100000 500000)
+#centers=(10000)
 
 for cen in ${centers[@]}; do
   wd=/N/u/sekanaya/sali/projects/flink/kmeans/data2
@@ -59,8 +59,9 @@ for cen in ${centers[@]}; do
   reportmpibindings=--report-bindings
   #reportmpibindings=
   #btl="-v --display-map --mca btl tcp,self,sm --mca btl_tcp_if_include eth1 --mca pml ob1"
-  btl="--mca btl tcp,self,sm"
+  #btl="--mca btl tcp,self,sm"
   #btl="--mca btl openib,self,sm"
+  btl="--mca btl ^tcp"
 
   if [ $procbind = "core" ]; then
       # with IB and bound to corresponding PEs
